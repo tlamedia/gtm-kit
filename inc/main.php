dfb9e97ab109d3ca9895ac2ca7a41cca52fffae3
Torben Lundsgaard <tl@tlamedia.dk>
2022-08-28 12:25:17 +0200

    Prefix function names with gtmkit_

--- inc/main.php
+++ inc/main.php
@@ -23,11 +23,11 @@ require GTMKIT_PATH . 'vendor/autoload.php';
 /**
  * Plugin activation hook.
  */
-function plugin_activation(): void {
+function gtmkit_plugin_activation(): void {
 	new Installation();
 	do_action( 'gtmkit_activate' );
 }
-register_activation_hook( GTMKIT_FILE, 'TLA_Media\GTM_Kit\plugin_activation' );
+register_activation_hook( GTMKIT_FILE, 'TLA_Media\GTM_Kit\gtmkit_plugin_activation' );
 
 /**
  * Add plugin action links on Plugins page.
@@ -36,11 +36,11 @@ register_activation_hook( GTMKIT_FILE, 'TLA_Media\GTM_Kit\plugin_activation' );
  *
  * @return array
  */
-function add_plugin_action_link( array $links ): array {
+function gtmkit_add_plugin_action_link( array $links ): array {
 
 	$custom['settings'] = sprintf(
 		'<a href="%s" aria-label="%s">%s</a>',
-		esc_url(  menu_page_url( 'gtmkit_settings', false ) ),
+		esc_url(  menu_page_url( 'gtmkit_general', false ) ),
 		esc_attr__( 'Go to GTK Kit Settings page', 'gtmkit' ),
 		esc_html__( 'Settings', 'gtmkit' )
 	);
@@ -51,14 +51,14 @@ function add_plugin_action_link( array $links ): array {
 /**
  * Load text domain for translation.
  */
-function load_text_domain(): void {
+function gtmkit_load_text_domain(): void {
 	load_plugin_textdomain( 'gtmkit', false, dirname( GTMKIT_BASENAME ) . '/languages/' );
 }
 
 /**
  * Load frontend.
  */
-function frontend_init(): void {
+function gtmkit_frontend_init(): void {
 	$options = new Options();
 	BasicDatalayerData::register( $options );
 	Frontend::register( $options );
@@ -71,7 +71,7 @@ function frontend_init(): void {
 /**
  * Load backend.
  */
-function admin_init(): void {
+function gtmkit_admin_init(): void {
 
 	if ( version_compare( get_option( 'gtmkit_version' ), GTMKIT_VERSION, '<' ) ) {
 		if ( function_exists( 'opcache_reset' ) ) {
@@ -93,11 +93,11 @@ function admin_init(): void {
 if ( ! wp_installing() ) {
 
 	if ( is_admin() ) {
-		add_action( 'plugins_loaded', 'TLA_Media\GTM_Kit\load_text_domain' );
-		add_action( 'plugins_loaded', 'TLA_Media\GTM_Kit\admin_init' );
-		add_filter( 'plugin_action_links_' . plugin_basename( GTMKIT_FILE ), 'TLA_Media\GTM_Kit\add_plugin_action_link', 10, 1 );
+		add_action( 'plugins_loaded', 'TLA_Media\GTM_Kit\gtmkit_load_text_domain' );
+		add_action( 'plugins_loaded', 'TLA_Media\GTM_Kit\gtmkit_admin_init' );
+		add_filter( 'plugin_action_links_' . plugin_basename( GTMKIT_FILE ), 'TLA_Media\GTM_Kit\gtmkit_add_plugin_action_link', 10, 1 );
 	} elseif ( ! wp_doing_ajax() ) {
-		add_action( 'plugins_loaded', 'TLA_Media\GTM_Kit\frontend_init' );
+		add_action( 'plugins_loaded', 'TLA_Media\GTM_Kit\gtmkit_frontend_init' );
 
 	}
 }
