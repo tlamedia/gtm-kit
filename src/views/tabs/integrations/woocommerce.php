a21c1b8fae716d17844ce73ec19561d4d626696c
Torben Lundsgaard <tl@tlamedia.dk>
2022-09-09 09:15:00 +0200

    Disable WooCommerce settings if WooCommerce is not active

--- src/views/tabs/integrations/woocommerce.php
+++ src/views/tabs/integrations/woocommerce.php
@@ -12,12 +12,25 @@ if ( ! defined( 'GTMKIT_VERSION' ) ) {
 	header( 'HTTP/1.1 403 Forbidden' );
 	exit();
 }
+
+$woocommerce_is_inactive = ! is_plugin_active( 'woocommerce/woocommerce.php' );
 ?>
 <div class="gtmkit-setting-row gtmkit-setting-row-heading gtmkit-clear">
 	<h2>
 		<?php esc_html_e( 'WooCommerce Integration', 'gtmkit' ); ?>
 	</h2>
-	<?php esc_html_e( 'The #1 open source eCommerce platform built for WordPress', 'gtmkit' ) . ': <a href="https://woocommerce.com/" target="_blank">WooCommerce</a>'; ?>
+	<p><?php esc_html_e( 'The #1 open source eCommerce platform built for WordPress', 'gtmkit' ) . ': <a href="https://woocommerce.com/" target="_blank">WooCommerce</a>'; ?></p>
+	<?php if ( $woocommerce_is_inactive ): ?>
+		<p>
+			<span class="error"><?php esc_html_e('WooCommerce is not installed', 'gtmkit');?></span>.
+			<?php
+			printf(
+				__('You can download %s here.', 'gtmkit'),
+				'<a href="https://wordpress.org/plugins/woocommerce/" target="_blank">WooCommerce</a>'
+			);
+			?>
+		</p>
+	<?php endif; ?>
 </div>
 
 <div class="gtmkit_section_message warning">
@@ -37,7 +50,11 @@ $form->setting_row(
 	'checkbox-toggle',
 	'woocommerce_integration',
 	__( 'Track WooCommerce', 'gtmkit' ),
-	[],
+	[
+		'attributes' => [
+			'disabled' => $woocommerce_is_inactive,
+		]
+	],
 	__( 'Choose this option if you would like to track e-commerce data.', 'gtmkit' )
 );
 
@@ -51,7 +68,12 @@ $taxonomies = get_taxonomies(
 	'and'
 );
 
-$field_data = [];
+$field_data = 	[
+	'attributes' => [
+		'disabled' => $woocommerce_is_inactive,
+	]
+]
+;
 $field_data['options'] = [];
 
 foreach ( $taxonomies as $taxonomy ) {
@@ -69,7 +91,11 @@ $form->setting_row(
 	'checkbox-toggle',
 	'woocommerce_use_sku',
 	__('Use SKU instead of ID', 'gtmkit'),
-	[],
+	[
+		'attributes' => [
+			'disabled' => $woocommerce_is_inactive,
+		]
+	],
 	__('Use SKU instead of the product ID with fallback to ID if no SKU is set.', 'gtmkit')
 );
 
@@ -77,7 +103,11 @@ $form->setting_row(
 	'checkbox-toggle',
 	'woocommerce_exclude_tax',
 	__('Exclude tax', 'gtmkit'),
-	[],
+	[
+		'attributes' => [
+			'disabled' => $woocommerce_is_inactive,
+		]
+	],
 	__('Exclude tax from prices and revenue', 'gtmkit')
 );
 
@@ -85,11 +115,19 @@ $form->setting_row(
 	'checkbox-toggle',
 	'woocommerce_exclude_shipping',
 	__('Exclude shipping from revenue', 'gtmkit'),
-	[],
+	[
+		'attributes' => [
+			'disabled' => $woocommerce_is_inactive,
+		]
+	],
 	__('Exclude shipping from revenue', 'gtmkit')
 );
 
-$field_data = [];
+$field_data = 	[
+	'attributes' => [
+		'disabled' => $woocommerce_is_inactive,
+	]
+];
 $field_data['options'] = [
 	1 => [
 		'label'      => __("When the 'Place order' button is clicked", 'gtmkit'),
@@ -111,7 +149,11 @@ $form->setting_row(
 	$field_data
 );
 
-$field_data = [];
+$field_data = 	[
+	'attributes' => [
+		'disabled' => $woocommerce_is_inactive,
+	]
+];
 $field_data['options'] = [
 	1 => [
 		'label'      => __("When the 'Place order' button is clicked", 'gtmkit'),
@@ -137,6 +179,10 @@ $form->setting_row(
 	'checkbox-toggle',
 	'woocommerce_dequeue_script',
 	__('Dequeue Default JS', 'gtmkit'),
-	[],
+	[
+		'attributes' => [
+			'disabled' => $woocommerce_is_inactive,
+		]
+	],
 	__('Enable this option to dequeue the default JavaScript if you plan to create your own JavaScript.', 'gtmkit')
 );
