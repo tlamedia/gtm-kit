048a81c6b4a7cbef979a666f0829a14731f45681
Torben Lundsgaard <tl@tlamedia.dk>
2022-09-30 22:21:11 +0200

    Prepare release

--- gtm-kit.php
+++ gtm-kit.php
@@ -3,7 +3,7 @@
  * Google Tag Manager Kit.
  *
  * Plugin Name: GTM Kit
- * Version:     1.2.1
+ * Version:     1.3
  * Plugin URI:  https://gtmkit.com/
  * Description: Google Tag Manager implementation focusing on flexibility and pagespeed.
  * Author:      TLA Media
@@ -27,7 +27,7 @@ if ( ! function_exists( 'add_filter' ) ) {
 	exit();
 }
 
-define( 'GTMKIT_VERSION', '1.2.1' );
+define( 'GTMKIT_VERSION', '1.3' );
 
 if ( ! defined( 'GTMKIT_FILE' ) ) {
 	define( 'GTMKIT_FILE', __FILE__ );
