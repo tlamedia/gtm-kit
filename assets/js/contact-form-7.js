51283b1efed7ce15f55688915e1a24d5b21e81c3
Torben Lundsgaard <tl@tlamedia.dk>
2022-10-10 15:47:41 +0200

    Fix custom datalayer name in CF7

--- assets/js/contact-form-7.js
+++ assets/js/contact-form-7.js
@@ -1,1 +1,1 @@
-document.addEventListener("wpcf7mailsent",function(t){window.dataLayer.push({event:"gtmkit.CF7MailSent",formId:t.detail.contactFormId,response:t.detail.inputs})});
+const datalayer_name=window.gtmkit_settings.datalayer_name;document.addEventListener("wpcf7mailsent",function(t){window[datalayer_name].push({event:"gtmkit.CF7MailSent",formId:t.detail.contactFormId,response:t.detail.inputs})});
