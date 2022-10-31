7c6251f95c4d314a8842c371af1f8538f3dc4aa1
Torben Lundsgaard <tl@tlamedia.dk>
2022-10-31 15:13:47 +0100

    Add edit button on container dashboard

--- src/views/tabs/general/dashboard.php
+++ src/views/tabs/general/dashboard.php
@@ -23,19 +23,33 @@ if ( ! defined( 'GTMKIT_VERSION' ) ) {
 					<?php esc_attr_e( 'Active', 'gtmkit' ); ?>
 				</span>
 				<div class="gtmkit-list-item-actions">
-					<p>
-						<?php esc_html_e( 'Container ID:', 'gtmkit'); ?>
-						<?php echo esc_html( Options::init()->get( 'general', 'gtm_id' ) ); ?>
-					</p>
+					<div class="gtmkit-list-item-description">
+						<p>
+							<?php esc_html_e( 'Container ID:', 'gtmkit'); ?>
+							<?php echo esc_html( Options::init()->get( 'general', 'gtm_id' ) ); ?>
+						</p>
+					</div>
+					<div class="gtmkit-list-item-buttons">
+						<a href="<?php echo admin_url( 'admin.php?page=gtmkit_general#top#container' ); ?>" id="gtmkit-open-tab-container" class="gtmkit-button gtmkit-open-tab">
+							<?php esc_html_e( 'Edit container', 'gtmkit' ); ?>
+						</a>
+					</div>
 				</div>
 				<?php else: ?>
 				<span class="gtmkit-list-item-pill gtmkit-list-item-pill-red">
 					<?php esc_attr_e( 'Inactive', 'gtmkit' ); ?>
 				</span>
 				<div class="gtmkit-list-item-actions">
-					<p>
-						<?php esc_html_e( 'The container is not active but the datalayer is generated.', 'gtmkit'); ?>
-					</p>
+					<div class="gtmkit-list-item-description">
+						<p>
+							<?php esc_html_e( 'The container is not active but the datalayer is generated.', 'gtmkit'); ?>
+						</p>
+					</div>
+					<div class="gtmkit-list-item-buttons">
+						<a href="<?php echo admin_url( 'admin.php?page=gtmkit_general#top#container' ); ?>" id="gtmkit-open-tab-container" class="gtmkit-button gtmkit-open-tab">
+							<?php esc_html_e( 'Edit container', 'gtmkit' ); ?>
+						</a>
+					</div>
 				</div>
 				<?php endif; ?>
 			</li>
