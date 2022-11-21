a3e7298632c1d9204e41e8ed4dd6147ea30ce175
Torben Lundsgaard <tl@tlamedia.dk>
2022-11-21 20:37:49 +0100

    Fix datalayer_name collision when both WC and CF7  are active

--- assets/js/contact-form-7.js
+++ assets/js/contact-form-7.js
@@ -1,1 +1,1 @@
-const datalayer_name=window.gtmkit_settings.datalayer_name;document.addEventListener("wpcf7mailsent",function(t){window[datalayer_name].push({event:"gtmkit.CF7MailSent",formId:t.detail.contactFormId,response:t.detail.inputs})});
+document.addEventListener("wpcf7mailsent",function(t){window[window.gtmkit_settings.datalayer_name].push({event:"gtmkit.CF7MailSent",formId:t.detail.contactFormId,response:t.detail.inputs})});
--- src/js/contact-form-7.js
+++ src/js/contact-form-7.js
@@ -1,7 +1,5 @@
-const datalayer_name = window["gtmkit_settings"].datalayer_name;
-
 document.addEventListener( 'wpcf7mailsent', function( event ) {
-	window[datalayer_name].push({
+	window[window["gtmkit_settings"].datalayer_name].push({
 		"event" : "gtmkit.CF7MailSent",
 		"formId" : event.detail.contactFormId,
 		"response" : event.detail.inputs
