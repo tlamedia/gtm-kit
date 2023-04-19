3676129c647f7e503a7923ea7f4e75917abeaa52
Torben Lundsgaard <tl@tlamedia.dk>
2023-04-19 13:20:48 +0200

    Prepare release 1.9

--- src/views/tabs/general/whats_new.php
+++ src/views/tabs/general/whats_new.php
@@ -20,12 +20,27 @@ if ( ! defined( 'GTMKIT_VERSION' ) ) {
 
 <div class="gtmkit_section_message">
 	<div class="stuffbox">
+		<h3 class="hndle">1.8: <?php esc_html_e( 'Include the customer data in the data layer', 'gtm-kit' ); ?></h3>
+		<div class="inside">
+			<p><?php esc_html_e( 'This release is the first of two steps to include customer data in the data layer for all events.', 'gtm-kit' ); ?></p>
+			<p><?php esc_html_e( 'You can now add the customer data to the "purchase" event.', 'gtm-kit' ); ?></p>
+			<p>
+				<a href="https://gtmkit.com/gtm-kit-1-9-include-the-customer-data-in-the-data-layer/#utm_source=gtmkit-plugin&utm_medium=software&utm_term=changelog&utm_content=release-notes-1-9">
+					<?php esc_html_e( 'Read about all the other change in the release Notes', 'gtm-kit' ); ?>
+				</a>
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="gtmkit_section_message">
+	<div class="stuffbox">
 		<h3 class="hndle">1.8: <?php esc_html_e( 'Support for the add_to_wishlist event', 'gtm-kit' ); ?></h3>
 		<div class="inside">
 			<p><?php esc_html_e( 'If you are using ‘YITH WooCommerce Wishlist’ or ‘TI WooCommerce Wishlist’ you can now track users who add a product to the wishlist. Between them theese two plugins have 1 million active installations.', 'gtm-kit' ); ?></p>
 			<p><?php esc_html_e( 'It requires no configuration and all you have to do is install and activate one of the two plugins.', 'gtm-kit' ); ?></p>
 			<p>
-				<a href="https://gtmkit.com/gtm-kit-1-8-support-for-the-add_to_wishlist-event//#utm_source=gtmkit-plugin&utm_medium=software&utm_term=changelog&utm_content=release-notes-1-8">
+				<a href="https://gtmkit.com/gtm-kit-1-8-support-for-the-add_to_wishlist-event/#utm_source=gtmkit-plugin&utm_medium=software&utm_term=changelog&utm_content=release-notes-1-8">
 					<?php esc_html_e( 'Read about all the other change in the release Notes', 'gtm-kit' ); ?>
 				</a>
 			</p>
