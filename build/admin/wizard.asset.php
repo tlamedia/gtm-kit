5e1861ce165207b8b7bda7a92b551f6eaaa5a81e
Torben Lundsgaard <tl@tlamedia.dk>
2023-08-03 15:40:13 +0200

    Update the setup wizard script

--- build/admin/wizard.asset.php
+++ build/admin/wizard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8be76677649a045a1840');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '66718e0acb5a9e6c03f7');
