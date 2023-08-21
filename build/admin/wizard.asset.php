1a1d2b7b8f5c09c806e26968b2e469e183d9f1cf
Torben Lundsgaard <tl@tlamedia.dk>
2023-08-21 17:21:01 +0200

    Added support for import of more plugin configurations

--- build/admin/wizard.asset.php
+++ build/admin/wizard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '66718e0acb5a9e6c03f7');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3936f8b956d5be2badb3');
