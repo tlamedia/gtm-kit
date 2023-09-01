a976a588febae0a8370006c94b25a1d509732ce0
Torben Lundsgaard <tl@tlamedia.dk>
2023-09-01 11:49:54 +0200

    Refactor code for simplicity

--- src/Frontend/BasicDatalayerData.php
+++ src/Frontend/BasicDatalayerData.php
@@ -51,8 +51,8 @@ final class BasicDatalayerData {
 	 */
 	public function get_datalayer_content( array $datalayer ): array {
 
-		$set_datalayer_post_type = $this->options->get( 'general', 'datalayer_post_type' );
-		$set_datalayer_page_type = $this->options->get( 'general', 'datalayer_page_type' );
+		$set_datalayer_post_type = (bool) $this->options->get( 'general', 'datalayer_post_type' );
+		$set_datalayer_page_type = (bool) $this->options->get( 'general', 'datalayer_page_type' );
 
 		if ( $set_datalayer_post_type ) {
 			$datalayer['pagePostType'] = get_post_type();
@@ -63,47 +63,7 @@ final class BasicDatalayerData {
 		}
 
 		if ( is_singular() ) {
-
-			global $post;
-
-			if ( $this->options->get( 'general', 'datalayer_categories' ) ) {
-				$post_categories = get_the_category();
-				if ( $post_categories ) {
-					foreach ( $post_categories as $category ) {
-						$datalayer['pageCategory'][] = $category->slug;
-					}
-				}
-			}
-
-			if ( $this->options->get( 'general', 'datalayer_tags' ) ) {
-				$post_tags = get_the_tags();
-				if ( $post_tags ) {
-					foreach ( $post_tags as $tag ) {
-						$datalayer['pageAttributes'][] = $tag->slug;
-					}
-				}
-			}
-
-			if ( $this->options->get( 'general', 'datalayer_post_title' ) ) {
-				$datalayer['postTitle'] = $post->post_title;
-			}
-
-			if ( $this->options->get( 'general', 'datalayer_post_id' ) ) {
-				$datalayer['postId'] = $post->ID;
-			}
-
-			if ( $this->options->get( 'general', 'datalayer_post_date' ) ) {
-				$datalayer['postDate'] = get_the_date( 'Y-m-d' );
-			}
-
-			if ( $this->options->get( 'general', 'datalayer_post_author_id' ) ) {
-				$author                  = get_userdata( $post->post_author );
-				$datalayer['authorName'] = $author->display_name;
-			}
-
-			if ( $this->options->get( 'general', 'datalayer_post_author_name' ) ) {
-				$datalayer['authorId'] = (int) $post->post_author;
-			}
+			$datalayer = $this->get_singular_datalayer_content( $datalayer );
 		}
 
 		if ( is_archive() || is_post_type_archive() ) {
@@ -116,35 +76,11 @@ final class BasicDatalayerData {
 		}
 
 		if ( is_search() ) {
-			if ( $set_datalayer_post_type ) {
-				$datalayer['pagePostType'] = 'search-results';
-			}
-			if ( $set_datalayer_page_type ) {
-				$datalayer['pageType'] = 'search-results';
-			}
-
-			global $wp_query;
-
-			$datalayer['siteSearchQuery']   = get_search_query();
-			$datalayer['siteSearchResults'] = $wp_query->found_posts;
+			$datalayer = $this->get_site_search_datalayer_content( $datalayer, $set_datalayer_post_type, $set_datalayer_page_type );
 		}
 
-		if ( is_front_page() ) {
-			if ( $set_datalayer_post_type ) {
-				$datalayer['pagePostType'] = 'frontpage';
-			}
-			if ( $set_datalayer_page_type ) {
-				$datalayer['pageType'] = 'frontpage';
-			}
-		}
-
-		if ( ! is_front_page() && is_home() ) {
-			if ( $set_datalayer_post_type ) {
-				$datalayer['pagePostType'] = 'blog_home';
-			}
-			if ( $set_datalayer_page_type ) {
-				$datalayer['pageType'] = 'blog_home';
-			}
+		if ( is_front_page() || is_home() ) {
+			$datalayer = $this->get_frontpage_datalayer_content( $datalayer, $set_datalayer_post_type, $set_datalayer_page_type );
 		}
 
 		if ( is_404() ) {
@@ -174,6 +110,117 @@ final class BasicDatalayerData {
 				$datalayer['pageType'] = $page_type;
 			}
 		}
+		return $datalayer;
+	}
+
+	/**
+	 * Get the dataLayer data for singular post types
+	 *
+	 * @param array $datalayer The datalayer.
+	 *
+	 * @return array
+	 */
+	private function get_singular_datalayer_content( array $datalayer ): array {
+
+		global $post;
+
+		if ( $this->options->get( 'general', 'datalayer_categories' ) ) {
+			$post_categories = get_the_category();
+			if ( $post_categories ) {
+				foreach ( $post_categories as $category ) {
+					$datalayer['pageCategory'][] = $category->slug;
+				}
+			}
+		}
+
+		if ( $this->options->get( 'general', 'datalayer_tags' ) ) {
+			$post_tags = get_the_tags();
+			if ( $post_tags ) {
+				foreach ( $post_tags as $tag ) {
+					$datalayer['pageAttributes'][] = $tag->slug;
+				}
+			}
+		}
+
+		if ( $this->options->get( 'general', 'datalayer_post_title' ) ) {
+			$datalayer['postTitle'] = $post->post_title;
+		}
+
+		if ( $this->options->get( 'general', 'datalayer_post_id' ) ) {
+			$datalayer['postId'] = $post->ID;
+		}
+
+		if ( $this->options->get( 'general', 'datalayer_post_date' ) ) {
+			$datalayer['postDate'] = get_the_date( 'Y-m-d' );
+		}
+
+		if ( $this->options->get( 'general', 'datalayer_post_author_id' ) ) {
+			$author                  = get_userdata( $post->post_author );
+			$datalayer['authorName'] = $author->display_name;
+		}
+
+		if ( $this->options->get( 'general', 'datalayer_post_author_name' ) ) {
+			$datalayer['authorId'] = (int) $post->post_author;
+		}
+
+		return $datalayer;
+	}
+
+	/**
+	 * Get the dataLayer data for the frontpage and home page
+	 *
+	 * @param array $datalayer The datalayer.
+	 * @param bool  $set_datalayer_post_type Whether the pagePostType property is active.
+	 * @param bool  $set_datalayer_page_type Whether the pageType property is active.
+	 *
+	 * @return array
+	 */
+	private function get_frontpage_datalayer_content( array $datalayer, bool $set_datalayer_post_type, bool $set_datalayer_page_type ): array {
+
+		if ( is_front_page() ) {
+			if ( $set_datalayer_post_type ) {
+				$datalayer['pagePostType'] = 'frontpage';
+			}
+			if ( $set_datalayer_page_type ) {
+				$datalayer['pageType'] = 'frontpage';
+			}
+		}
+
+		if ( ! is_front_page() && is_home() ) {
+			if ( $set_datalayer_post_type ) {
+				$datalayer['pagePostType'] = 'blog_home';
+			}
+			if ( $set_datalayer_page_type ) {
+				$datalayer['pageType'] = 'blog_home';
+			}
+		}
+
+		return $datalayer;
+	}
+
+	/**
+	 * Get the dataLayer data for site search
+	 *
+	 * @param array $datalayer The datalayer.
+	 * @param bool  $set_datalayer_post_type Whether the pagePostType property is active.
+	 * @param bool  $set_datalayer_page_type Whether the pageType property is active.
+	 *
+	 * @return array
+	 */
+	private function get_site_search_datalayer_content( array $datalayer, bool $set_datalayer_post_type, bool $set_datalayer_page_type ): array {
+
+		if ( $set_datalayer_post_type ) {
+			$datalayer['pagePostType'] = 'search-results';
+		}
+		if ( $set_datalayer_page_type ) {
+			$datalayer['pageType'] = 'search-results';
+		}
+
+		global $wp_query;
+
+		$datalayer['siteSearchQuery']   = get_search_query();
+		$datalayer['siteSearchResults'] = $wp_query->found_posts;
+
 		return $datalayer;
 	}
 }
