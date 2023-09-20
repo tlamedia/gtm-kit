f355ffd233bb67f36485d17e7440e051a528f14d
Torben Lundsgaard <tl@tlamedia.dk>
2023-09-20 13:56:00 +0200

    Require WordPress 6.2

--- gtm-kit.php
+++ gtm-kit.php
@@ -11,7 +11,7 @@
  * Text Domain: gtm-kit
  * Domain Path: /languages/
  * License:     GPLv3
- * Requires at least: 6.1
+ * Requires at least: 6.2
  * Requires PHP: 7.2
  *
  * WC requires at least: 7.6
