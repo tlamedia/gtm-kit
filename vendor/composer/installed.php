76396c756b6976e73c9b7c90c2915cf640ef555b
Torben Lundsgaard <tl@tlamedia.dk>
2023-10-06 10:55:17 +0200

    Prepare release 1.14.1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'tlamedia/gtm-kit',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'e4aaef6e023da58d9703ba3c905aab00342a79d6',
+        'reference' => '4892b79e515edc43c03a211a3abb22a12fcdf974',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'tlamedia/gtm-kit' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'e4aaef6e023da58d9703ba3c905aab00342a79d6',
+            'reference' => '4892b79e515edc43c03a211a3abb22a12fcdf974',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
