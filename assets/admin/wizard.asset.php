a592cbd93a0302446b0e8cb0ff2cee636e2ee2f7
Torben Lundsgaard <tl@tlamedia.dk>
2023-10-24 23:03:11 +0200

    Update tailwind

--- assets/admin/wizard.asset.php
+++ assets/admin/wizard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '67db6caf40f92ac1b922');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b0a66c663984544a3765');
