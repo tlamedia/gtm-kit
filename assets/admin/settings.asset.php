ebb6bbaf9a9e33aa9aa88b2085861a5deb800cd6
Torben Lundsgaard <tl@tlamedia.dk>
2023-11-14 13:52:22 +0100

    Update assets for admin settings

--- assets/admin/settings.asset.php
+++ assets/admin/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f982d86fe41bc860cf23');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '95a0f6a91825b279991e');
