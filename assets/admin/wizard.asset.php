1a1ae16cd843545c98472567e3382d4b6c563e4b
Torben Lundsgaard <tl@tlamedia.dk>
2023-11-23 23:17:06 +0100

    Update assets for admin settings

--- assets/admin/wizard.asset.php
+++ assets/admin/wizard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9329a881caa7395971bf');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f4e2c66c51685108a403');
