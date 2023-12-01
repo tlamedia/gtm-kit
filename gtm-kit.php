0cc6fdc1b88f0eea4ff76b41ef8db18fad7648b7
Torben Lundsgaard <tl@tlamedia.dk>
2023-12-01 08:26:19 +0100

    Prepare release 1.17

--- gtm-kit.php
+++ gtm-kit.php
@@ -3,7 +3,7 @@
  * GTM Kit Plugin
  *
  * Plugin Name: GTM Kit
- * Version:     1.17.0-beta.1
+ * Version:     1.17
  * Plugin URI:  https://gtmkit.com/
  * Description: Google Tag Manager implementation focusing on flexibility and pagespeed.
  * Author:      TLA Media
@@ -27,7 +27,7 @@ if ( ! function_exists( 'add_filter' ) ) {
 	exit();
 }
 
-const GTMKIT_VERSION = '1.17.0-beta.1';
+const GTMKIT_VERSION = '1.17';
 
 if ( ! defined( 'GTMKIT_FILE' ) ) {
 	define( 'GTMKIT_FILE', __FILE__ );
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'tlamedia/gtm-kit',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'd9835a2a11462e9314d9933b8413bb18fba4111b',
+        'reference' => '4215d9c063a2943f35f3d5d560f9e64c8d2801d1',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'tlamedia/gtm-kit' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'd9835a2a11462e9314d9933b8413bb18fba4111b',
+            'reference' => '4215d9c063a2943f35f3d5d560f9e64c8d2801d1',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
