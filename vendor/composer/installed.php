10538e83b1e8c2db9bd4e4d6c240761b95876c9c
Torben Lundsgaard <tl@tlamedia.dk>
2024-01-16 08:58:44 +0100

    Update dependencies

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'tlamedia/gtm-kit',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'cfde4bb434472c8c469284795c75b4479c67d526',
+        'reference' => 'd87d4c1a5f42801c9361110ef5b80cf794243a6f',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'tlamedia/gtm-kit' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'cfde4bb434472c8c469284795c75b4479c67d526',
+            'reference' => 'd87d4c1a5f42801c9361110ef5b80cf794243a6f',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
