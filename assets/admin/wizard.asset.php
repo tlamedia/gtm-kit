d5777d703e6cbc65407612997f501fe839877cd2
Torben Lundsgaard <tl@tlamedia.dk>
2024-03-08 16:10:04 +0100

    Update assets

--- assets/admin/wizard.asset.php
+++ assets/admin/wizard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fa4589566d6a7e354484');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '06a65e9ee5547f00712d');
