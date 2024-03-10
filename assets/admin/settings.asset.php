69868f14f2e09fcd4a9fa1a091acb68e61dac241
Torben Lundsgaard <tl@tlamedia.dk>
2024-03-10 12:44:12 +0100

    Update assets

--- assets/admin/settings.asset.php
+++ assets/admin/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'abe3b7caed39d37c83e8');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '53fcd2206b7b02fb47f5');
