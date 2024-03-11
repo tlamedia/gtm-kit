001e0949980711390785510a7f1fbb71bbcdee13
Torben Lundsgaard <tl@tlamedia.dk>
2024-03-11 16:45:36 +0100

    Activation notice on first install

--- assets/admin/wizard.asset.php
+++ assets/admin/wizard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '06a65e9ee5547f00712d');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '50640e302b51985018ae');
