567bb8267fbbc7849d8555f898e91c96882c02ba
Torben Lundsgaard <tl@tlamedia.dk>
2024-03-18 15:33:09 +0100

    Prepare release 1.20

--- assets/admin/wizard.asset.php
+++ assets/admin/wizard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dd644c94009f73f7d662');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e7d58fb1352372f542b9');
