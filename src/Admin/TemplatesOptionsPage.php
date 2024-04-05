06b94b32faf8e59071d89a64571232402249910d
Torben Lundsgaard <tl@tlamedia.dk>
2024-04-05 17:38:50 +0200

    Dynamic template sections

--- src/Admin/TemplatesOptionsPage.php
+++ src/Admin/TemplatesOptionsPage.php
@@ -89,11 +89,50 @@ final class TemplatesOptionsPage extends AbstractOptionsPage {
 				'currentPage'     => $page_slug,
 				'root'            => \esc_url_raw( rest_url() ),
 				'nonce'           => \wp_create_nonce( 'wp_rest' ),
+				'templates'       => $this->get_templates(),
 				'dashboardUrl'    => \menu_page_url( 'gtmkit_general', false ),
 				'integrationsUrl' => \menu_page_url( 'gtmkit_integrations', false ),
 				'templatesUrl'    => \menu_page_url( 'gtmkit_templates', false ),
 				'settings'        => $this->options->get_all_raw(),
 			]
 		);
+	}
+
+	/**
+	 * Get the templates
+	 *
+	 * @return array
+	 */
+	private function get_templates(): array {
+		$transient = 'gtmkit_templates';
+		$templates = get_transient( $transient );
+
+		if ( ! WP_DEBUG && $templates !== false ) {
+			return $templates;
+		}
+
+		$url = 'https://app.gtmkit.com/api/v1/get-templates';
+
+		if ( \is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
+			$url = add_query_arg( 'woo', 1, $url );
+		}
+
+		$url      = add_query_arg( 'woo', 1, $url );
+		$response = wp_remote_get( $url );
+
+		if ( is_wp_error( $response ) ) {
+			return [];
+		}
+
+		$json      = wp_remote_retrieve_body( $response );
+		$templates = json_decode( $json, true );
+
+		if ( json_last_error() !== JSON_ERROR_NONE ) {
+			return [];
+		}
+
+		set_transient( $transient, $templates, 12 * HOUR_IN_SECONDS );
+
+		return $templates;
 	}
 }
