2e53abe426da43839c56d01fe4e01eb9d242030d
Torben Lundsgaard <tl@tlamedia.dk>
2024-04-06 12:35:57 +0200

    Add missing version info

--- src/Admin/HelpOptionsPage.php
+++ src/Admin/HelpOptionsPage.php
@@ -92,6 +92,7 @@ final class HelpOptionsPage extends AbstractOptionsPage {
 				'dashboardUrl'    => \menu_page_url( 'gtmkit_general', false ),
 				'integrationsUrl' => \menu_page_url( 'gtmkit_integrations', false ),
 				'settings'        => $this->options->get_all_raw(),
+				'site_data'       => [ 'gtmkit_version' => GTMKIT_VERSION ],
 			]
 		);
 	}
