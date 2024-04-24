db2e53afe1e8d13e15a4a18cc3ebbe2cd0a5de41
Torben Lundsgaard <tl@tlamedia.dk>
2024-04-24 16:08:38 +0200

    Advanced GCM settings

--- assets/admin/settings.asset.php
+++ assets/admin/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '596c1a7b6bd34fb67624');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b217c3ad3a8b8bd93902');
