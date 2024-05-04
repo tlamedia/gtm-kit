7a8c74a1f63b10384b6e717a62449d3685eb0453
Torben Lundsgaard <tl@tlamedia.dk>
2024-05-04 16:04:24 +0200

    Add Event Inspector

--- assets/admin/settings.asset.php
+++ assets/admin/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b217c3ad3a8b8bd93902');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '316691c4b188eb4e0d33');
---
+++ assets/frontend/event-inspector.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '279d2a8bbf86dcc0b63b');
