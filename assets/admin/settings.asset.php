c90e110f0a263565e91c2038cbda7bc35990f9bd
Torben Lundsgaard <tl@tlamedia.dk>
2024-07-01 15:45:13 +0200

    Improve flexibility of integrations

--- assets/admin/settings.asset.php
+++ assets/admin/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2238b13f608a72073e6c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '9fc0bb35edc014b69389');
