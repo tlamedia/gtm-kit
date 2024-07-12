8116bd3738f2fa4d8c35c917b42c9435f78d223b
Torben Lundsgaard <tl@tlamedia.dk>
2024-07-12 11:51:16 +0200

    Prepare release 1.23.1

--- assets/admin/settings.asset.php
+++ assets/admin/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'b6ebb50b63ddf5ed55de');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1ca0dab2fa79f8b6fbd8');
