cea90415c68f0f9e25bc885617c1ec6ed3af1b79
Torben Lundsgaard <tl@tlamedia.dk>
2024-08-30 15:05:42 +0200

    Add standardised conditionals

---
+++ src/Common/Conditionals/ContactForm7Conditional.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * GTM Kit plugin file.
+ *
+ * @package GTM Kit
+ */
+
+namespace TLA_Media\GTM_Kit\Common\Conditionals;
+
+/**
+ * Conditional that is only met when Easy Digital Downloads is active.
+ */
+class ContactForm7Conditional implements Conditional {
+
+	/**
+	 * Returns `true` when the Contact Form 7 plugin is installed and activated.
+	 *
+	 * @return bool `true` when the Contact Form 7 plugin is installed and activated.
+	 */
+	public function is_met(): bool {
+		return \class_exists( 'CF7' );
+	}
+}
