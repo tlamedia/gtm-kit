bc46bf19b5a9a1f384932060093a6ca1e2fb3615
Torben Lundsgaard <tl@tlamedia.dk>
2024-10-08 16:59:53 +0200

    Update documentation

--- src/Common/Conditionals/PremiumConditional.php
+++ src/Common/Conditionals/PremiumConditional.php
@@ -8,14 +8,14 @@
 namespace TLA_Media\GTM_Kit\Common\Conditionals;
 
 /**
- * Conditional that is only met when WooCommerce is active.
+ * Conditional that is only met when GTM Kit Premium is active.
  */
 class PremiumConditional implements Conditional {
 
 	/**
-	 * Returns `true` when the WooCommerce plugin is installed and activated.
+	 * Returns `true` when the GTM Kit Premium plugin is installed and activated.
 	 *
-	 * @return bool `true` when the WooCommerce plugin is installed and activated.
+	 * @return bool `true` when the Premium plugin is installed and activated.
 	 */
 	public function is_met(): bool {
 		return \defined( 'GTMKIT_WOO_FILE' ) || \defined( 'GTMKIT_PREMIUM_FILE' );
