3ee42d45623957a57ece69ba7ac3416fb5e824aa
Torben Lundsgaard <tl@tlamedia.dk>
2025-01-23 14:09:28 +0100

    Updated to PHPStan 2.0

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'tlamedia/gtm-kit',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '940a0f1643dfafeedc6fa994a1f82a4e09848797',
+        'reference' => '74bb599aa4e6d8c41968da7ce0e9d3be63ed2b10',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'tlamedia/gtm-kit' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '940a0f1643dfafeedc6fa994a1f82a4e09848797',
+            'reference' => '74bb599aa4e6d8c41968da7ce0e9d3be63ed2b10',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
