6dcdb2d706acb8380d9460384d66c6d6c5d62ed0
Torben Lundsgaard <tl@tlamedia.dk>
2025-04-01 16:59:53 +0200

    Prepare release 2.3.2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'tlamedia/gtm-kit',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '28b39822035526424f406254d41a7060e988065b',
+        'reference' => '5e9a5d2b672646a6181354ffc06b6d9352bf36c0',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'tlamedia/gtm-kit' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '28b39822035526424f406254d41a7060e988065b',
+            'reference' => '5e9a5d2b672646a6181354ffc06b6d9352bf36c0',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
