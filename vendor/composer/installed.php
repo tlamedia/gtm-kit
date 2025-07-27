cbb745484d758a110063eec724bd75884c79781d
Torben Lundsgaard <tl@tlamedia.dk>
2025-07-27 14:16:48 +0200

    Update EDD requirement

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'tlamedia/gtm-kit',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'bd2fd10ac60c9aa7a2d4104bbd775cc54d7e67cc',
+        'reference' => '42e6c4f04a4ba20350f750d4272c0eb5c22f6c12',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'tlamedia/gtm-kit' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'bd2fd10ac60c9aa7a2d4104bbd775cc54d7e67cc',
+            'reference' => '42e6c4f04a4ba20350f750d4272c0eb5c22f6c12',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
