e852add4a3937e2caf643e294409e376858a301f
Torben Lundsgaard <tl@tlamedia.dk>
2025-08-12 09:06:57 +0200

    Update WPCS

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'tlamedia/gtm-kit',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '5ea8688e7048f73584022140e91d7e7fa72d09d9',
+        'reference' => '563df2855f0b73be67d0323c200e44566c7fa532',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'tlamedia/gtm-kit' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '5ea8688e7048f73584022140e91d7e7fa72d09d9',
+            'reference' => '563df2855f0b73be67d0323c200e44566c7fa532',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
