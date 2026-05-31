dcdd2c5c48e302092aa0904be12cb79603fd5614
Torben Lundsgaard <tl@tlamedia.dk>
2026-05-31 16:41:01 +0200

    Build: subscription event toggles in the WooCommerce settings page
    Rebuilt gtm-kit-settings admin bundle carrying the WooCommerce
    Subscriptions per-event webhook toggles. Source change lives in the
    gtm-kit-settings repository.

--- assets/admin/settings.asset.php
+++ assets/admin/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '9a5626d41e6394bdfc76');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '35f955b53fba8473197a');
